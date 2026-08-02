96fa0322727965440268c53bee97ea0f13a84e77
Achal_313 <achaldahatonde@gmail.com>
2026-08-02 23:16:33 +0530

    add 29 assignment

---
+++ 29Assignmeny/Modules/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace _29Jul.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string VehicleName { get; set; }
+        public string Brand { get; set; }
+        public int ModelYear { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ 29Assignmeny/Services/VehicleServices.cs
@@ -0,0 +1,58 @@
+﻿using _29Jul.Models;
+
+namespace _29Jul.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private static List<Vehicle> vehicles = new List<Vehicle>()
+        {
+            new Vehicle
+            {
+                Id = 1,
+                VehicleName = "Swift",
+                Brand = "Maruti",
+                ModelYear = 2023,
+                Price = 800000
+            },
+
+            new Vehicle
+            {
+                Id = 2,
+                VehicleName = "Creta",
+                Brand = "Hyundai",
+                ModelYear = 2024,
+                Price = 1500000
+            },
+
+            new Vehicle
+            {
+                Id = 3,
+                VehicleName = "Nexon",
+                Brand = "Tata",
+                ModelYear = 2024,
+                Price = 1200000
+            }
+        };
+
+        public List<Vehicle> GetVehicles()
+        {
+            return vehicles;
+        }
+
+        public Vehicle? GetVehicle(int id)
+        {
+            return vehicles.FirstOrDefault(v => v.Id == id);
+        }
+
+        public Vehicle? GetVehicleByName(string name)
+        {
+            return vehicles.FirstOrDefault(v => v.VehicleName == name);
+        }
+
+        public Vehicle AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+            return vehicle;
+        }
+    }
+}
